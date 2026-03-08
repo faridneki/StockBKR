79dd802cf17618ce7d47422d61d325bd2fd6eece
faridneki <pro.benazzouz@gmail.com>
2026-03-08 11:54:11 +0100

    feat: transforme l'app en PWA

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
+
+// Utilisation de require au lieu de import
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators: false,
   eslint: {
-    // Attention: cela désactive ESLint pendant le build en production
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
